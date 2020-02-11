99762685597714a54d3d2b7e665bb32e8b8c277c
motty93 <rdwbocungelt5@gmail.com>
2020-02-11 22:27:27 +0900

    update: routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#index'
+
+  namespace :api do
+    namespace :azure_active_directories do
+      post :authorization
+      get :callback
+    end
+  end
 end
